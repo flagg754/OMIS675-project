8ea8dcad36569720c04193e6ea4c474157f4aca1
flagg754 <slind2@gmail.com>
2015-10-12 18:28:02 -0500

    Commit - login page mockup Added - slate-bootswatch

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OMIS675_Project.Startup))]
+namespace OMIS675_Project
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
